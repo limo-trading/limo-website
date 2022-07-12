52ecd9dab4ef7e9704d35f1e0d503252f645eda6
polycloud-dev <heidenreichdd@gmail.com>
2022-07-12 20:38:51 +0200

    added providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
 import '../styles/globals.css'
+import { useState } from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { getCookie, setCookies } from 'cookies-next';
+
+import { MantineProvider, ColorSchemeProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+
+export default function App(props) {
+
+  const { Component, pageProps } = props;
+
+  const [colorScheme, setColorScheme] = useState(props.colorScheme);
+
+  const toggleColorScheme = (value) => {
+    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
+    setColorScheme(nextColorScheme);
+    setCookies('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
+  };
+
+  return <>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{colorScheme}} withGlobalStyles withNormalizeCSS >
+        <NotificationsProvider>
+          <Component {...pageProps} />
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  </>
 }
 
-export default MyApp
+App.getInitialProps = ({ ctx }) => ({
+  colorScheme: getCookie('mantine-color-scheme', ctx) || 'dark',
+});
